403ed2992d2b66cc8f080dec45e3157bd7d33e01
mikkimat81539 <mikkimat81539@gmail.com>
2026-07-03 20:32:47 +0100

    created vertical containers with buttons

--- Alarm/containerTest.py
+++ Alarm/containerTest.py
@@ -16,8 +16,44 @@ class MainWindow(QMainWindow):
 
 		# Layout Container
 		self.container = QWidget()
-		self.layout = QGridLayout(self.container)
+		self.layout = QVBoxLayout(self.container)
 		self.setCentralWidget(self.container)
+
+		self.vertical_layout()
+
+		self.horizontal_layout()
+
+	def vertical_layout(self):
+		self.v_container = QWidget()
+		self.v_layout = QVBoxLayout(self.v_container)
+		self.v_container.setStyleSheet("background-color: red")
+
+		self.v_container.setFixedSize(300, 200)
+
+		self.layout.addWidget(self.v_container)
+
+		self.V_Buttons()
+
+	def V_Buttons(self):
+		self.button1 = QPushButton("1")
+		self.button2 = QPushButton("2")
+		self.button3 = QPushButton("3")
+		self.button4 = QPushButton("4")	
+
+		self.button1.setCursor(Qt.CursorShape.PointingHandCursor)
+		self.button2.setCursor(Qt.CursorShape.PointingHandCursor)
+		self.button3.setCursor(Qt.CursorShape.PointingHandCursor)
+		self.button4.setCursor(Qt.CursorShape.PointingHandCursor)
+
+
+		self.v_layout.addWidget(self.button1)
+		self.v_layout.addWidget(self.button2)
+		self.v_layout.addWidget(self.button3)
+		self.v_layout.addWidget(self.button4)
+
+		
+	def horizontal_layout(self):
+		pass
 
 	
 app = QApplication([])
