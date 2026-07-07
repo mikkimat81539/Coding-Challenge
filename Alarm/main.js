997c0f4878d10428a15b1062c82b75906cb1bf88
mikkimat81539 <mikkimat81539@gmail.com>
2026-07-07 05:33:29 +0100

    created local time variables

--- Alarm/main.js
+++ Alarm/main.js
@@ -1,16 +1,19 @@
-// Check to see if AM or PM is selected or stored in local storage or T/F -- DONE
+/* Check to see if AM or PM is selected or stored in local storage or T/F -- DONE
 
-// If am is selected hour should be (0 - 11) -- DONE
-// if pm is selected hour should be (12 - 23) -- DONE
+If am is selected hour should be (0 - 11) -- DONE
+if pm is selected hour should be (12 - 23) -- DONE
 
-// EX: if 12:00 AM than 00:00 -- DONE
-// EX: if 6:00 PM than 18:00 -- DONE
+EX: if 12:00 AM than 00:00 -- DONE
+EX: if 6:00 PM than 18:00 -- DONE
 
-// grab the value of the hour and if True (PM) display (12 - 23) -- DONE
+grab the value of the hour and if True (PM) display (12 - 23) -- DONE
 
-// Store hour and minutes in format ???
-// if localTime == scheduled time
+Store hour and minutes in format -- DONE
+if localTime == scheduled time 
 
+*/
+
+// Define Variables
 const am_pm = document.getElementById("sunrise"); 
 const sendBtn = document.getElementById("send")
 const userHour = document.getElementById("hour")
@@ -26,6 +29,11 @@ function sunTime() {
 	let int_min = Number(userMin.value)
 
 	timeBool = (sunValue === "am") // if user selects AM than True
+
+	const localHour = new Date().getHours()
+	const localMin = new Date().getMinutes()
+
+	const localTime = `${String(localHour)}:${String(localMin)}`
 
 	// PM should be (12 - 23)
 	if (!timeBool && int_hour !== 12) {
